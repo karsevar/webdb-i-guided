d8f37f4509107a751f47a3621911a1ee5c609d14
masonkarsevar <masonkarsevar@gmail.com>
2019-09-09 10:59:37 -0700

    post request finished

--- posts/post-router.js
+++ posts/post-router.js
@@ -10,7 +10,10 @@ router.get('/', (req, res) => {
     // select * from posts;
 
     // all db operations return a promise
-    db.select('*').from('posts') 
+    // A shortcut for the below command is db('posts')
+    // You can also select particular columns using this short cut
+    // db('posts').select('title', 'contents')
+    db.select('id', 'title', 'contents').from('posts') 
         .then(results => {
             res.status(200).json(results);
         })
@@ -20,11 +23,30 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    const {id} = req.params;
 
+    db('posts').where({id})
+        .first()
+        .then(results => {
+            res.status(200).json(results)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
 router.post('/', (req, res) => {
+    // insert into posts () values () 
+    const postData = req.body;
+    // validate the data before inserting into db
 
+    db('posts').insert(postData, 'id')
+        .then(([results]) => {
+            res.status(200).json(results);
+        })
+        .catch(error => {
+            res.json(error);
+        })
 });
 
 router.put('/:id', (req, res) => {
